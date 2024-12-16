d584f44ea9b9b4906b0231e1c80ebe44ee1d4216
KCoder <102844052+krishna7054@users.noreply.github.com>
2024-12-16 18:52:23 +0530

    Update ProfilePage.js

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -75,11 +75,11 @@ const ProfilePage = () => {
     <div className="  min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8 m-2 group px-10 py-5 bg-white/10 rounded-lg flex flex-col items-center justify-center gap-2 relative after:absolute after:h-full after:bg-[#abd373] z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0 [&amp;_p]:delay-200 [&amp;_p]:transition-all">
       <div className="bg-white rounded-lg shadow-lg p-6 sm:p-8 lg:p-10 max-w-xl w-full">
         <div className="flex flex-col items-center">
-    <div className="w-32 h-32 rounded-full object-cover mb-4> {user.name[0]}</div>
-          // {user.profilePicture && (
-            // <img src={user.profilePicture} alt="Profile" className="w-32 h-32 rounded-full object-cover mb-4" />
+    
+          {user.profilePicture && (
+            <img src={user.profilePicture} alt="Profile" className="w-32 h-32 rounded-full object-cover mb-4" />
             
-          // )}
+          )}
   
           <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold text-gray-800 mb-2">{user.name}'s Profile</h2>
           <p className="text-gray-600 mb-4  text-xl sm:text-lg md:text-xl">{user.profession}</p>
